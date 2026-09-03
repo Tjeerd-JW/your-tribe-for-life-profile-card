d0ea3e83b90c1c2609e553823506d09ae8aaabfe
Tjeerd Wokke <tj.wokke@gmail.com>
2026-09-03 12:46:08 +0200

    Get data from directus test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
+<script>
+    let { data } = $props();
+
+    const persons = data.persons;
+
+    console.log(persons)
+</script>
+
+{#each persons as person }
+    <p>{person.name} - {person.fav_movie}</p>
+
+{/each}
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+</p>
