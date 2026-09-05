223ad08bc578588e11be394fdba5e51ff3fef3b3
Tjeerd <tj.wokke@gmail.com>
2026-09-05 15:14:41 +0200

    basic rotation added #3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,60 @@
 <script>
-    let { data } = $props();
+    // let { data } = $props();
+    // const persons = data.persons;
+    let rotateX = $state(0);
+    let rotateY = $state(0);
+    let sidePosition = $state(2);
+    let currentFace = $state('front')
 
-    const persons = data.persons;
+    function turnHandler(direction) {
+        let cube = document.querySelector(".cube");
 
-    console.log(persons);
+        switch (direction) {
+            case "top":
+                rotateX -=  90;
+                break;
+            case "left":
+                rotateY += 90;
+                break;
+            case "right":
+                rotateY -= 90;
+                break;
+            case "bottom":
+                rotateX += 90;
+                break;
+        }
+    }
 </script>
 
-<div class="scene">
-    <div class="cube">
-        <div class="face front"></div>
-        <div class="face right"></div>
-        <div class="face back"></div>
-        <div class="face left"></div>
-        <div class="face top"></div>
-        <div class="face bottom"></div>
+<section>
+    <button class="top-button" onclick={() => turnHandler("top")}>top</button>
+    <button class="left-button" onclick={() => turnHandler("left")}>left</button
+    >
+    <button class="right-button" onclick={() => turnHandler("right")}
+        >right</button
+    >
+    <button class="bottom-button" onclick={() => turnHandler("bottom")}
+        >bottom</button
+    >
+
+    <div class="scene" style={`--side-position: ${sidePosition};`}>
+        <div
+            class="cube"
+            style={`--rotate-X: ${rotateX}deg; --rotate-Y: ${rotateY}deg;`}
+        >
+            <div class="face front">front</div>
+            <div class="face right">right</div>
+            <div class="face back">back</div>
+            <div class="face left">left</div>
+            <div class="face top">top</div>
+            <div class="face bottom">bottom</div>
+        </div>
     </div>
-</div>
+</section>
 
 <style>
     .scene {
         --cube-size: 512px;
-        --side-position: 2;
 
         .cube {
             width: var(--cube-size);
@@ -29,7 +63,10 @@
             top: 50%;
             left: 50%;
             transform-style: preserve-3d;
-            transform: translate(-50%, -50%) rotateX(240deg) rotateY(120deg);
+            transform: translate(-50%, -50%) rotateX(var(--rotate-X))
+                rotateY(var(--rotate-Y));
+
+            transition: ease-in-out 2s;
             .face {
                 width: var(--cube-size);
                 height: var(--cube-size);
@@ -61,12 +98,13 @@
         background-color: green;
     }
     .top {
-        transform: rotateX(-90deg)
+        transform: rotateX(90deg)
             translateZ(calc(var(--cube-size) / var(--side-position)));
         background-color: pink;
     }
     .bottom {
-        transform: rotateX(90deg) translateZ(calc(var(--cube-size) / var(--side-position)));
+        transform: rotateX(-90deg)
+            translateZ(calc(var(--cube-size) / var(--side-position)));
         background-color: orange;
     }
 </style>
